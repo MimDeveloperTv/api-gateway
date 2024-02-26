2ad0ca0e572dd030dbb5a4ea6650541b39f4b776
mimdeveloper <mimdeveloper.tv@gmail.com>
2024-02-26 17:13:14 +0330

    Install And Config Passport And MyConsole Commands And TEST #WIP

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Auth\AuthController as Auth;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+/* -----  API Routes --------------------------------------------------------------------- */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
+    Route::post('/login',[Auth::class,'login'])->name('auth.login');
+    Route::post('/register',[Auth::class,'register'])->name('auth.register');
+    Route::post('/logout',[Auth::class,'logout'])->name('auth.logout')->middleware('auth:api');
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
